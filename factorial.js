2591f28e447327ceae89b3a37ca878ac61ba563a
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-06 01:02:13 +0600

    factorial

---
+++ factorial.js
@@ -0,0 +1,22 @@
+// 5 * 4 * 3 * 2 * 1
+
+/* let factorial = 1;
+for(let i = 4; i >= 1; i--){
+    // factorial = factorial * i;
+    factorial =  i  * factorial;
+}
+console.log(factorial);
+ */
+
+
+//
+
+function factorial(i){
+    if(i == 1){
+        return 1;
+    }
+    // return factorial(--i) * i  ;
+    return i * factorial(--i);
+}
+
+console.log(factorial(4));
